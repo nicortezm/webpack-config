72741149853de7f2e6cd3c74e6f8f5ae1e95c7d5
Nicolas <nascorman@gmail.com>
2021-03-26 16:43:54 -0300

    TodoList class Creada

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import { Todo } from './classes/todo.class.js';
-// import { saludar } from './js/componentes.js';
+// import { TodoList } from './classes/todo-list.class.js';
+// import { Todo } from './classes/todo.class.js';
+
+import { Todo,TodoList } from './classes'
+
 import './styles.css';
 
+const todoList = new TodoList();
 
 const tarea = new Todo('Aprender Javascript');
+const tarea2 = new Todo('Comprar un unicornio');
 
-console.log(tarea);
+todoList.nuevoTodo( tarea);
+todoList.nuevoTodo( tarea2);
+console.log(todoList);
