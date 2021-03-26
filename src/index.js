2b5d10e3e8e7d3ba1b9088e5fcbd3a5c2167241f
Nicolas <nascorman@gmail.com>
2021-03-26 17:58:52 -0300

    Agreado Evento texto

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { crearTodoHtml } from './js/componentes';
 
 import './styles.css';
 
-const todoList = new TodoList();
+export const todoList = new TodoList();
 
 const tarea = new Todo('Aprender Javascript');
 todoList.nuevoTodo( tarea);
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,7 +1,12 @@
 
+import { Todo } from '../classes';
+
+import { todoList } from '../index';
 // Referencias enn el HTML
 
 const divTodoList = document.querySelector('.todo-list');
+const txtInput = document.querySelector('.new-todo');
+
 
 export const crearTodoHtml = ( todo ) => {
     const htmlTodo =`
@@ -22,3 +27,18 @@ export const crearTodoHtml = ( todo ) => {
     return div.firstElementChild;
 
 }
+
+
+// Eventos
+
+txtInput.addEventListener('keyup',( event )=> {
+
+    if (event.keyCode === 13 && txtInput.value.length > 0) {
+
+        console.log(txtInput.value);
+        const nuevoTodo = new Todo(txtInput.value);
+        todoList.nuevoTodo(nuevoTodo);
+        crearTodoHtml( nuevoTodo);
+        txtInput.value = '';
+    }
+})
