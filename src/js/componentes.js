4fd9cac797f392b63b5a3d979cc45e3e1f14b7f0
Nicolas <nascorman@gmail.com>
2021-03-26 21:18:39 -0300

    Eliminar todosCompletados

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -6,6 +6,8 @@ import { todoList } from '../index';
 
 const divTodoList = document.querySelector('.todo-list');
 const txtInput = document.querySelector('.new-todo');
+const btnBorrar = document.querySelector('.clear-completed');
+
 
 
 export const crearTodoHtml = ( todo ) => {
@@ -57,5 +59,19 @@ divTodoList.addEventListener('click', (event) => {
         todoList.eliminarTodo(todoId);
         divTodoList.removeChild(todoElemento);
         
+    }
+});
+
+btnBorrar.addEventListener('click', () =>{
+
+    todoList.eliminarCompletados();
+    
+    for(let i = divTodoList.children.length-1;i>= 0;i-- ){
+
+        const elemento = divTodoList.children[i];
+
+        if (elemento.classList.contains('completed')) {
+            divTodoList.removeChild(elemento);
+        }
     }
 });
