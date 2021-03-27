c3d18c3c922be9fc8c3e763caa1ec5f01ad4a0f3
Nicolas <nascorman@gmail.com>
2021-03-27 14:58:54 -0300

    Creado filtros todos

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -7,7 +7,8 @@ import { todoList } from '../index';
 const divTodoList = document.querySelector('.todo-list');
 const txtInput = document.querySelector('.new-todo');
 const btnBorrar = document.querySelector('.clear-completed');
-
+const ulFilters = document.querySelector('.filters');
+const AnchorFilters = document.querySelectorAll('.filtro');
 
 
 export const crearTodoHtml = ( todo ) => {
@@ -74,4 +75,37 @@ btnBorrar.addEventListener('click', () =>{
             divTodoList.removeChild(elemento);
         }
     }
+});
+
+ulFilters.addEventListener('click',(event) => {
+
+    const filtro = event.target.text;
+    if (!filtro) {return;}
+
+    AnchorFilters.forEach(elem =>  elem.classList.remove('selected'));
+
+    event.target.classList.add('selected');
+    for (const elemento of divTodoList.children) {
+
+        elemento.classList.remove('hidden');
+        const completado = elemento.classList.contains('completed');
+
+        switch (filtro) {
+            case 'Pendientes':
+                if (completado) {
+                    elemento.classList.add('hidden');
+                }
+                break;
+            case 'Completados':
+                if (!completado) {
+                    elemento.classList.add('hidden');
+                }
+                break;
+            default:
+                break;
+        }
+        
+    }
+        
+    
 });
