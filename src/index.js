43ad5d7fc295782c82d847442a0a9ca0951c5fb1
Nicolas <nascorman@gmail.com>
2021-03-28 21:48:19 -0300

    manejo errores await

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import {obtenerHeroesArr} from './js/await';
+import {obtenerHeroesArr,obtenerHeroeAwait} from './js/await';
 
 console.time('await');
 
 
-obtenerHeroesArr().then( heroes => {
-    console.table(heroes);
+obtenerHeroeAwait('capi2')
+    .then( heroe => {
+        console.log(heroe);
 
-    console.timeEnd('await');
-});
+        console.timeEnd('await');
+    }).catch(console.warn);
--- src/js/await.js
+++ src/js/await.js
@@ -21,3 +21,24 @@ const heroesPromesas = heroesIds.map( buscarHeroe );
 export const obtenerHeroesArr = async() => {
     return await Promise.all( heroesIds.map( buscarHeroe ) );
 };
+
+
+export const obtenerHeroeAwait = async(id) => {
+    
+    try {
+
+        const heroe = await buscarHeroeAsync(id);
+        return heroe;
+        
+    } catch (err) {
+
+        console.log('CATCH!!!!');
+        return {
+            nombre: 'Sin nombre',
+            poder: 'Sin poder'
+        };
+        
+    }
+
+    
+}
