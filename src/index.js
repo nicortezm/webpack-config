2ade32a2fbc3fdba6aa886a6368e4681cb655d9f
Nicolas <nascorman@gmail.com>
2021-03-29 12:16:52 -0300

    crud terminado

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@
 
 import * as CRUD from './js/crud-provider';
 
-CRUD.getUsuario(4).then(console.log);
+// CRUD.getUsuario(4).then(console.log);
 
-CRUD.crearUsuario({
-    name: 'Nicolas',
-    job: 'Pescador'
-}).then(console.log);
+// CRUD.crearUsuario({
+//     name: 'Nicolas',
+//     job: 'Pescador'
+// }).then(console.log);
+
+// CRUD.actualizarUsuario(1,{
+//     name:'Hola',
+//     job: 'Mundo'}).then(console.log);
+
+CRUD.borrarUsuario(1).then(console.log);
--- src/js/crud-provider.js
+++ src/js/crud-provider.js
@@ -24,10 +24,34 @@ const crearUsuario = async( usuario ) => {
     return await resp.json();
 }
 
+const actualizarUsuario = async( id, usuario ) => {
+    
+    const resp = await fetch(`${urlCRUD}/${id}`,{
+        method: 'PUT',
+        body: JSON.stringify(usuario),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    return await resp.json();
+}
+
+const borrarUsuario = async(id) => {
+    
+    const resp = await fetch(`${urlCRUD}/${id}`,{
+        method:'DELETE'
+    });
+
+    return (resp.ok) ? 'Borrado': 'No se pudo eliminar' ;
+}
+
 
 
 
 export{
     getUsuario,
-    crearUsuario
+    crearUsuario,
+    actualizarUsuario,
+    borrarUsuario
 }
