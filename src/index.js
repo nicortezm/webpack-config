f2169b92d05f1463d32c6d6b61b77184d6c082d5
Nicolas <nascorman@gmail.com>
2021-03-29 00:00:17 -0300

    Obtener chistes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
+import { init } from "./js/chistes-page";
 
-
-const jokeUrl = 'https://api.chucknorris.io/jokes/random';
-
-// fetch(jokeUrl).then((resp) => {
-//     resp.json().then(({id,value}) => {
-//         console.log( id );
-//         console.log( value );
-//     })
-// })
-
-fetch(jokeUrl)
-        .then(resp => resp.json())
-        .then(({id,value}) =>{
-            console.log(id , value);
-        })
+init();
---
+++ src/js/http-provider.js
@@ -0,0 +1,27 @@
+
+const jokeUrl = 'https://api.chucknorris.io/jokes/random';
+
+
+const obtenerChiste = async(params) => {
+
+    try {
+        const  resp = await fetch(jokeUrl);
+
+        if (!resp.ok) throw ('No se pudo realizar la peticion');
+
+        const {icon_url,id,value} = await resp.json();
+
+        return {icon_url,id,value} ;
+
+    } catch (err ) {
+        
+        throw err;
+    }
+    
+    
+}
+
+
+export {
+    obtenerChiste
+}
